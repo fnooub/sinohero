283eb7b916cc57d9882fe7e352b4cf8419f7a79b
fnooub <61742373+fnooub@users.noreply.github.com>
2022-07-02 08:03:32 -0700

    Add files via upload

--- chap.php
+++ chap.php
@@ -1,5 +1,5 @@
 <?php
-	
+	include 'Paginate.php';
 	$file = 'http://res.cloudinary.com/fivegins/raw/upload/v1656517312/luufiles/Tr%E1%BB%9F_Th%C3%A0nh_Th%C3%A1nh_Nh%C3%A2n_L%C3%A0_Lo%E1%BA%A1i_G%C3%AC_Tr%E1%BA%A3i_Nghi%E1%BB%87m_1_eay2jm.txt';
 	$nd = file_get_contents($file);
 
@@ -7,7 +7,7 @@
 	array_shift($data);
 	$total = count($data);
 
-	include 'Paginate.php';
+
 	$config['current_page'] = isset($_GET['c']) ? $_GET['c'] : 1;
 	$config['total_rows'] = $total;
 	$config['base_url'] = 'chap.php?c=(:num)';
